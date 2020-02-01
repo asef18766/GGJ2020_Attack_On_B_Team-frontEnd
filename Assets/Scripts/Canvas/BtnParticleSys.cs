345b145b87103d1b556cc39d9a2ce8632711fccb
Wolvz <peng86david@gmail.com>
2020-02-01 17:43:23 +0800

    Start scene transition

---
+++ Assets/Scripts/Canvas/BtnParticleSys.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnParticleSys : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps  = GetComponent<ParticleSystem>();
+        ps.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayPS(){
+        ps.Play();
+    }
+
+    public void PausePS(){
+        ps.Stop();
+    }
+}
