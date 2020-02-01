e561e1c71fe1503217ed72a2e736c363fd9b1d78
bogay <pojay11523@gmail.com>
2020-02-01 15:58:53 +0800

    add player move, update notification center

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : Entity
+{
+    public string characterName;
+    public int maxHP;
+    public int resourceCapacity;
+    public float speed;
+    private int hp;
+    private int resource;
+
+    public void Attack(int weaponId) {}
+    public void Build(string entityType, Vector3 loc) {}
+    public void Collect() {}
+    public void Damage() {}
+    public void purchase(string itemName) {}
+}
---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    public Character character;
+}
